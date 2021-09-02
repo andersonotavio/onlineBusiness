7f7fabf9969b8aae52bcc93a9d77ba172c052a66
andersonotavio <andersonotavio@hotmail.com>
2021-09-02 16:53:15 -0300

    Search area

--- src/helpers/OnbAPI.js
+++ src/helpers/OnbAPI.js
@@ -75,6 +75,13 @@ const OnbAPI = {
       '/states'
     )
     return json.states;
+  },
+
+  getCategories: async () =>{
+    const json = await apiFetchGet(
+      '/categories'
+    )
+    return json.categories
   }
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
+import { PageArea, SearchArea } from './styled';
+import useApi from '../../helpers/OnbAPI'
+
+import { PageContainer } from '../../components/MainComponents';
 
 const Home = () => {
+
+  const api = useApi()
+
+  const [listStates, setStatesList] = useState([])
+  const [categories, setCategories] = useState([])
+
+  useEffect(()=>{
+    const getStates =  async () => {
+      const list = await api.getStates()
+      setStatesList(list)
+    }
+    getStates()
+  }, [])
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const cat = await api.getCategories()
+      setCategories(cat)
+    }
+    getCategories()
+  }, [])
+
+
   return (
-    <div>
-      <h1>Page Home</h1>
-      <Link to="/about">About</Link>
-    </div>
+   <>
+      <SearchArea>
+        <PageContainer>
+          <div className="searchBox">
+            <form method="GET" action="/ads">
+              <input type="text" name="q" placeholder="O que você procura?" />
+              <select name="state">
+                {listStates.map((i,k) =>
+                  <option key={k} value={i.name}>{i.name}</option>
+                )}
+              </select>
+              <button>Pesquisar</button>
+            </form>
+          </div>
+          <div className="categoryList">
+            {categories.map((i, k) =>
+              <Link key={k} to={`/ads?cat=${i.slug}`} className="categoryItem">
+                <img src={i.img} alt="" />
+                <span>{i.name}</span>
+              </Link>
+            )}
+          </div>
+        </PageContainer>
+      </SearchArea>
+      <PageContainer>
+        <PageArea>
+          ...
+        </PageArea>
+      </PageContainer>
+   </>
   )
 }
 
-export default Home; 
+export default Home;
