c76a5c4fb8fcfe2b73c67869310d53e55b4f3e6b
andersonotavio <andersonotavio@hotmail.com>
2021-09-08 20:05:41 -0300

    fake loading

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { PageArea } from './styled';
+import { Fake, PageArea } from './styled';
 import useApi from '../../helpers/OnbAPI'
 import { useParams } from 'react-router';
 
@@ -20,20 +20,21 @@ const AdPage = () => {
         <div className="sideLeft">
           <div className="box">
             <div className="adImg">
-              ...
+            { loading && <Fake height={300} />}
             </div>
             <div className="adInfo">
               <div className="adName">
-                ...
+               { loading && <Fake height={20} />}
               </div>
               <div className="adDescription">
-              ...
+              { loading && <Fake height={100} />}
               </div>
             </div>
           </div>
         </div>
         <div className="sideRight">
-          ...
+          <div className="box box--padding"> { loading && <Fake height={20} />}</div>
+          <div className="box  box--padding"> { loading && <Fake height={50} />}</div>
         </div>
       </PageArea>
     </PageContainer>
